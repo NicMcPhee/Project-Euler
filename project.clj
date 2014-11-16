892ebc694118c75541ff9ba8ed6c70a95b6f47ba
Nic McPhee <mcphee@morris.umn.edu>
2014-11-15 21:08:38 -0500

    Set up initial leiningens project structure.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject project-euler "0.1.0-SNAPSHOT"
+  :description "My (Clojure) solutions to Project Euler problems"
+  :url "https://github.com/NicMcPhee/Project-Euler"
+  :license {:name "MIT  License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
